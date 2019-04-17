674b4c87f6831fd4400d0b760cd6d5cf90c2c646
Edison Alba <edison.alba@imaginamos.com.co>
2019-04-17 09:45:59 -0500

    pasar esta tienda a nuestros componentes

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { createStore } from 'redux'
+import postReducer from './redux/reducers/postReducer'
+import { Provider } from 'react-redux';
 
-const store = createStore()
+const store = createStore(postReducer)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/reducers/postReducer.js
+++ src/redux/reducers/postReducer.js
@@ -1,5 +1,13 @@
 const postReducer = (state = [], action) => {
 
+    //acciones para cambiar el estado 
+    switch(action.type){
+        case 'ADD_POST':
+            return state.concat([action.data])
+        default:
+            return state
+    }
+
 }
 
 export default postReducer
